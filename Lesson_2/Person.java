bd1a7f3fc7380c690a64bd22d193983ae196e01f
Vadim-I <isaeff@bk.ru>
2020-07-13 18:53:38 +0700

    Homework_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+	String name = "Pavel";
+	String sex = "Man";
+	float height = 1.84f;
+	float weight = 88.5f;
+	int age = 29;
+
+	void move() {
+		System.out.println("Pavel может ходить");
+	}
+
+	void sit() {
+		System.out.println("Pavel может сидеть");
+	}
+
+	boolean run() {
+		System.out.println("Pavel сейчас бежит");
+		return true;
+	}
+
+	void speak() {
+		System.out.println("Pavel умеет говорить");
+	}
+
+	String learnJava() {
+		System.out.println("Pavel учит Java");
+		return "At the beginning";
+	}
+}
