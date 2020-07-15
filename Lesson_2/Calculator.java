cdab33bfd569f05a3cb6ed28c7fb378c2395955c
Vadim-I <isaeff@bk.ru>
2020-07-15 09:24:12 +0700

    Added changes_1

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,26 +1,56 @@
 public class Calculator {
-	int number1;
-	int number2;
-	int result;
-	char sign;
+	private int number1;
+	private int number2;
+	private int result;
+	private char sign;
 
-	int calcResult(int number1, int number2, char sign) {
+	public int getNumber1() {
+		return number1;
+	}
+
+	void setNumber1(int number1) {
+		this.number1 = number1;
+	}
+
+	public int getNumber2() {
+		return number2;
+	}
+
+	void setNumber2(int number2) {
+		this.number2 = number2;
+	}
+
+	public char getSign() {
+		return sign;
+	}
+
+	void setSign(char sign) {
+		this.sign = sign;
+	}
+
+	int calculate(int number1, int number2, char sign) {
 		switch (sign) {
-		    case '+':	result = number1 + number2;
-		    			break;
-		    case '-':	result = number1 - number2;
-		    			break;
-		    case '*':	result = number1 * number2;
-		    			break;
-		    case '/':	result = number1 / number2;
-		    			break;
-		    case '^':	result = 1;
-		    			for(int i = 1; i <= number2; i++) {
-		    		  		result *= number1;
-		    		  	}
-		    		  	break;
-		    case '%':	result = number1 % number2;
-		    			break;
+			case '+':
+				result = number1 + number2;
+				break;
+			case '-':
+				result = number1 - number2;
+				break;
+			case '*':
+				result = number1 * number2;
+				break;
+			case '/':
+				result = number1 / number2;
+				break;
+			case '^':
+				result = 1;
+				for(int i = 1; i <= number2; i++) {
+					result *= number1;
+				}
+				break;
+			case '%':
+				result = number1 % number2;
+				break;
 		} return result;
 	}
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 	public static void main(String[] args) {
-	    Calculator myCalc = new Calculator();
+		Calculator myCalc = new Calculator();
 		char calcAgain = '0';
 
 		while(calcAgain != 'n') {
 			System.out.print("Введите первое число: ");
-		    Scanner scan0 = new Scanner(System.in);
-			myCalc.number1 = scan0.nextInt();
+			Scanner scan0 = new Scanner(System.in);
+			myCalc.setNumber1(scan0.nextInt());
 			System.out.print("Введите знак математической операции (+, -, *, /, ^, %): ");
 			Scanner scan1 = new Scanner(System.in);
-			myCalc.sign = scan1.next().charAt(0);
+			myCalc.setSign(scan1.next().charAt(0));
 			System.out.print("Введите второе число: ");
-		    Scanner scan2 = new Scanner(System.in);
-			myCalc.number2 = scan2.nextInt();
+			Scanner scan2 = new Scanner(System.in);
+			myCalc.setNumber2(scan2.nextInt());
 
-			System.out.println("Результат вычисления: " + myCalc.calcResult(myCalc.number1, myCalc.number2, myCalc.sign));
+			System.out.println("Результат вычисления: " + myCalc.calculate(myCalc.getNumber1(), myCalc.getNumber2(), myCalc.getSign()));
 			System.out.println("Хотите продолжить? (y / n)");
 			Scanner scan3 = new Scanner(System.in);
 			calcAgain = scan3.next().charAt(0);
