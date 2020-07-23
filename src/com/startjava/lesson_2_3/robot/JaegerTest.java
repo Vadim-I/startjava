9258d82a03a33650c7befe432b8092b3d0adce06
Vadim-I <isaeff@bk.ru>
2020-07-23 08:44:21 +0700

    Homework_3

--- src/com/startjava/lesson_2_3/robot/JaegerTest.java
+++ src/com/startjava/lesson_2_3/robot/JaegerTest.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.robot;
+
 public class JaegerTest {
 	public static void main(String[] args) {
 		Jaeger crimsonTyphoon = new Jaeger("Crimson Typhoon", "Mark-4", "China", "Destroyed", 76.2f, 1.722f, 9, 8, 6);
