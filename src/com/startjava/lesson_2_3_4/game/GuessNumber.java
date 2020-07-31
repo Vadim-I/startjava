0faebc9ec857a513b7023cdb8306e485b091ae46
Vadim-I <isaeff@bk.ru>
2020-07-31 10:56:25 +0700

    Homework_4 / changes_3

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -15,53 +15,47 @@ public class GuessNumber {
 	}
 
 	public void startGame() {
-		pcNum = (int)(Math.random() * 101);
-		int index = -1;
+		pcNum = (int) (Math.random() * 101);
 		System.out.println("Начинаем игру. У вас 10 попыток.");
-		while(index < 9) {
-			index++;
-			enterNums(player1, index);
-			if(compareNums(player1, index)) {
+		while(player1.getIndex() < 9) {
+			enterNum(player1);
+			if(compareNums(player1)) {
 				break;
 			}
-			enterNums(player2, index);
-			if(compareNums(player2, index)) {
+			enterNum(player2);
+			if(compareNums(player2)) {
 				break;
 			}
 		}
 		System.out.println("Введенные игроками числа:");
 		printNums(player1);
 		printNums(player2);
-		fillNums(player1);
-		fillNums(player2);
+		player1.fillNums();
+		player2.fillNums();
 	}
 
-	private void enterNums(Player player, int index) {
+	private void enterNum(Player player) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Введите число игрока " + player.getName() + ": ");
-		player.setNum(scan.nextInt(), index);
+		player.setNum(scan.nextInt());
 	}
 
-	private boolean compareNums(Player player, int index) {
-		if (player.getNum(index) > pcNum) {
+	private boolean compareNums(Player player) {
+		if (player.getNum() > pcNum) {
 			System.out.println("Это число больше того, что загадал компьютер.");
-		} else if (player.getNum(index) < pcNum) {
+		} else if (player.getNum() < pcNum) {
 			System.out.println("Это число меньше того, что загадал компьютер.");
 		} else {
-			System.out.println("Игрок " + player.getName() + " угадал число " + pcNum + " с " + (index+1) + " попытки!");
+			System.out.println("Игрок " + player.getName() + " угадал число " + pcNum + " с " + (player.getIndex()+1) + " попытки!");
 			return true;
 		}
-		if ((index == 9) && (player.getNum(index) != pcNum)) {
+		if ((player.getIndex() == 9) && (player.getNum() != pcNum)) {
 			System.out.println("У игрока " + player.getName() + " закончились попытки.");
 		}
 		return false;
 	}
 
-    private void printNums(Player player) {
-		System.out.println(Arrays.toString(Arrays.copyOf(player.getNums(), player.getIndex()+1)));
-    }
-
-    private void fillNums(Player player) {
-		Arrays.fill(player.setNums(player.getNums()), 0, player.getIndex()+1, 0);
-    }
+	private void printNums(Player player) {
+		System.out.println(Arrays.toString(player.getNums()));
+	}
 }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -6,7 +6,7 @@ public class Player {
 
 	private String name;
 	private int[] nums = new int[10];
-	private int index;
+	private int index = -1;
 
 	public Player(String name) {
 		this.name = name;
@@ -16,25 +16,25 @@ public class Player {
 		return name;
 	}
 
-	public int getNum(int index) {
+	public int getNum() {
 		return nums[index];
 	}
 
-	public void setNum(int number, int index) {
-		this.nums[index] = number;
-		this.index = index;
+	public void setNum(int number) {
+		index++;
+		nums[index] = number;
 	}
 
 	public int[] getNums() {
-		return Arrays.copyOf(nums, nums.length);
-	}
-
-	public int[] setNums(int[] nums) {
-		this.nums = nums;
-		return nums;
+		return Arrays.copyOf(nums, index+1);
 	}
 
 	public int getIndex() {
 		return index;
+	}
+
+	public void fillNums() {
+		Arrays.fill(Arrays.copyOf(nums, nums.length), 0, index+1, 0);
+		index = -1;
 	}
 }
