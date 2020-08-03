bb163cbe311a5b37e550a6c9ae09bb97f540df6f
Vadim-I <isaeff@bk.ru>
2020-08-03 11:48:59 +0700

    Homework_5

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -7,6 +7,6 @@ VALUES
 ('November Ajax',	'Mark-6',	'78.94',	'1.934',	'Destroyed',	'USA',			'2018-04-17',	'1'),
 ('Obsidian Fury',	'Mark-5',	'82.15',	'2.188',	'Active',	'China',		'2035-01-25',	'0'),
 ('Romeo Blue',		'Mark-1',	'77.72',	'7.775',	'Destroyed',	'USA',			'2015-12-15',	'2'),
-('Saber Athena',	'Mark-7',	'76.91',	'1.628',	'Active',	'Japan',			'2033-12-20',	'1'),
-('Striker Eureka',	'Mark-5',	'76.20',	'1.850',	'Destroyed',	'Australia',	'2019-11-02',	'11'),
+('Saber Athena',	'Mark-7',	'76.91',	'1.628',	'Active',	'Japan',		'2033-12-20',	'1'),
+('Striker Eureka',	'Mark-5',	'76.20',	'1.850',	'Destroyed',	'Australia',		'2019-11-02',	'11'),
 ('Tacit Ronin',		'Mark-1',	'74.37',	'7.450',	'Destroyed',	'Japan',		'2015-12-06',	'3');
