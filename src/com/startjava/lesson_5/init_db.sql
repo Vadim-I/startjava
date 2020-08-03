02267a0f27418a2423d6e79311f18b4d30a3c595
Vadim-I <isaeff@bk.ru>
2020-08-03 11:45:07 +0700

    Homework_5

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,12 +1,12 @@
 INSERT INTO Robots(modelName, mark, height, weight, status, origin, launch, kaijuKill)
 VALUES
-('Bracer Phoenix',	'Mark-5',	'70.91',	'2.128',	'Destroyed',	'China',		'2025-11-01',	'1'),
-('Coyote Tango',	'Mark-1',	'85.34',	'2.312',	'Destroyed',	'Japan',		'2015-12-30',	'2'),
-('Crimson Typhoon',	'Mark-4',	'76.20',	'1.722',	'Destroyed',	'China',		'2018-08-22',	'7'),
-('Gipsy Avenger',	'Mark-6',	'81.77',	'2.004',	'Active',		'Australia',	'2034-01-12',	'1'),
-('November Ajax',	'Mark-6',	'78.94',	'1.934',	'Destroyed',	'USA',			'2018-04-17',	'1'),
-('Obsidian Fury',	'Mark-5',	'82.15',	'2.188',	'Active',		'China',		'2035-01-25',	'0'),
-('Romeo Blue',		'Mark-1',	'77.72',	'7.775',	'Destroyed',	'USA',			'2015-12-15',	'2'),
-('Saber Athena',	'Mark-7',	'76.91',	'1.628',	'Active',		'Japan',		'2033-12-20',	'1'),
-('Striker Eureka',	'Mark-5',	'76.20',	'1.850',	'Destroyed',	'Australia',	'2019-11-02',	'11'),
-('Tacit Ronin',		'Mark-1',	'74.37',	'7.450',	'Destroyed',	'Japan',		'2015-12-06',	'3');
+('Bracer Phoenix',	'Mark-5',	'70.91',	'2.128',	'Destroyed',	'China',	'2025-11-01',	'1'),
+('Coyote Tango',	'Mark-1',	'85.34',	'2.312',	'Destroyed',	'Japan',	'2015-12-30',	'2'),
+('Crimson Typhoon',	'Mark-4',	'76.20',	'1.722',	'Destroyed',	'China',	'2018-08-22',	'7'),
+('Gipsy Avenger',	'Mark-6',	'81.77',	'2.004',	'Active',		'Australia','2034-01-12',	'1'),
+('November Ajax',	'Mark-6',	'78.94',	'1.934',	'Destroyed',	'USA',		'2018-04-17',	'1'),
+('Obsidian Fury',	'Mark-5',	'82.15',	'2.188',	'Active',		'China',	'2035-01-25',	'0'),
+('Romeo Blue',		'Mark-1',	'77.72',	'7.775',	'Destroyed',	'USA',		'2015-12-15',	'2'),
+('Saber Athena',	'Mark-7',	'76.91',	'1.628',	'Active',		'Japan',	'2033-12-20',	'1'),
+('Striker Eureka',	'Mark-5',	'76.20',	'1.850',	'Destroyed',	'Australia','2019-11-02',	'11'),
+('Tacit Ronin',		'Mark-1',	'74.37',	'7.450',	'Destroyed',	'Japan',	'2015-12-06',	'3');
